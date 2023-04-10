bc9f0229675f6fad552600f12165f360a674f6ef
Vadim Alexeev <thewiz5@yandex.ru>
2023-04-10 23:58:09 +0300

    creating checker_user_location for telegram bot

--- program.py
+++ program.py
@@ -1,31 +1,79 @@
+# import requests
+# import time
+
+
+# API_url : str = 'https://api.telegram.org/bot'
+# API_CATS_url : str = 'https://aws.random.cat/meow'
+# BOT_token = '6201426279:AAEqpUOSWWIAoss33W46k1N7TKhRQbImCVI'
+# ERROR_text = 'Извините, *здесь* должны были быть котики :()'
+
+# offset : int = -2
+# counter : int = 0
+# cat_link : str
+
+# while counter < 100:
+#     print('attemp = ', counter)
+#     updates = requests.get(f'{API_url}{BOT_token}/getUpdates?offset={offset + 1}').json()
+#     # print(updates['result'])
+#     if updates['result']:
+#         for result in updates['result']:
+#             offset = result['update_id']
+#             chat_id = result['message']['from']['id']
+
+#         cat_response = requests.get(API_CATS_url)
+#         if cat_response.status_code == 200:
+#             cat_link = cat_response.json()['file']
+#             requests.get(f'{API_url}{BOT_token}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
+#         else:
+#             requests.get(f'{API_url}{BOT_token}/sendMessage?chat_id={chat_id}&text={ERROR_text}')
+
+#     time.sleep(1)
+#     counter += 1
+
 import requests
 import time
+import json
+
+TOKEN = '6201426279:AAEqpUOSWWIAoss33W46k1N7TKhRQbImCVI'
+URL = 'https://api.telegram.org/bot'
+
+def get_updates(offset=0):
+    result = requests.get(f'{URL}{TOKEN}/getUpdates?offset={offset}').json()
+    return result['result']
+
+def send_message(chat_id, text):
+    requests.get(f'{URL}{TOKEN}/sendMessage?chat_id={chat_id}&text={text}')
+
+def reply_keyboard(chat_id, text):
+    reply_markup ={ "keyboard": [["Привет", "Hello"], [{"request_location":True, "text":"Где я нахожусь"}]], "resize_keyboard": True, "one_time_keyboard": True}
+    data = {'chat_id': chat_id, 'text': text, 'reply_markup': json.dumps(reply_markup)}
+    requests.post(f'{URL}{TOKEN}/sendMessage', data=data)
+
+def check_message(chat_id, message):
+    if message.lower() in ['привет', 'hello']:
+        send_message(chat_id, 'Привет :)')
+    else:
+        reply_keyboard(chat_id, 'Я не понимаю тебя :(')
+
+def run():
+    update_id = get_updates()[-1]['update_id'] # Сохраняем ID последнего отправленного сообщения боту
+    while True:
+        time.sleep(2)
+        messages = get_updates(update_id) # Получаем обновления
+        for message in messages:
+            # Если в обновлении есть ID больше чем ID последнего сообщения, значит пришло новое сообщение
+            if update_id < message['update_id']:
+                update_id = message['update_id']# Сохраняем ID последнего отправленного сообщения боту
+                if (user_message := message['message'].get('text')): # Проверим, есть ли текст в сообщении
+                    check_message(message['message']['chat']['id'], user_message) # Отвечаем
+                if (user_location := message['message'].get('location')): # Проверим, если ли location в сообщении
+                    print(user_location)
+                    print(round(user_location['latitude'], 1))
+                    if round(user_location['latitude'], 1) > 44 and round(user_location['latitude'], 1) < 45:
+                        send_message(message['message']['chat']['id'], 'ок')
+                    else:
+                        send_message(message['message']['chat']['id'], 'так се')
 
 
-API_url : str = 'https://api.telegram.org/bot'
-API_CATS_url : str = 'https://aws.random.cat/meow'
-BOT_token = '6201426279:AAEqpUOSWWIAoss33W46k1N7TKhRQbImCVI'
-ERROR_text = 'Извините, *здесь* должны были быть котики :()'
-
-offset : int = -2
-counter : int = 0
-cat_link : str
-
-while counter < 100:
-    print('attemp = ', counter)
-    updates = requests.get(f'{API_url}{BOT_token}/getUpdates?offset={offset + 1}').json()
-    # print(updates['result'])
-    if updates['result']:
-        for result in updates['result']:
-            offset = result['update_id']
-            chat_id = result['message']['from']['id']
-
-        cat_response = requests.get(API_CATS_url)
-        if cat_response.status_code == 200:
-            cat_link = cat_response.json()['file']
-            requests.get(f'{API_url}{BOT_token}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-        else:
-            requests.get(f'{API_url}{BOT_token}/sendMessage?chat_id={chat_id}&text={ERROR_text}')
-
-    time.sleep(1)
-    counter += 1
+if __name__ == '__main__':
+    run()
