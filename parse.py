350fd1b871338ae44b68521d154120b0b337eab2
Vadim Alexeev <thewiz5@yandex.ru>
2023-06-28 16:18:19 +0300

    init

--- parse.py
+++ parse.py
@@ -11,23 +11,22 @@ def str_time_prop(start, end, time_format, prop):
 
     return time.strftime(time_format, time.localtime(ptime))
 
-
 def random_date(start, end, prop):
     return str_time_prop(start, end, '%Y-%m-%d', prop)
     
-def get_photo_from_nasa(args):
+def get_photo_from_nasa():
     data_json = requests.get(f'https://api.nasa.gov/planetary/apod?date={random_date("2016-1-1", "2023-1-1", random.random())}&api_key=DEMO_KEY')
     data = json.loads(data_json.text)
 
     return data.get('url')
  
+
 token = '6050659800:AAHPcJ1bs_ZfMvxorDIWnrpb5VWakVA34ew'
 bot = telebot.TeleBot(token)
 
+
 @bot.message_handler(commands=['day'])
 def get_everyday_photo(message):
-    bot.send_message(chat_id=message.chat.id, text='wait...')
-
     try:
         bot.send_photo(chat_id=message.chat.id, photo=get_photo_from_nasa(1), timeout=5)
     except:
--- writer.py
+++
@@ -1,3 +0,0 @@
-d = (i**2 for i in range(1, 6))
-a = max(d)
-b = min(d)
